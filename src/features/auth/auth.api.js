7eacfcb64593f507e5f23c464a36e60a4527c9c6
zagaofficework-bit <zagaofficework@gmail.com>
2026-04-06 09:47:12 +0530

    api calls

---
+++ src/features/auth/auth.api.js
@@ -0,0 +1,2 @@
+import apiClient from "../../services/apiClient";
+
---
+++ src/services/apiClient.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+
+const apiClient = axios.create({
+    baseURL: "https://www.zagainstitute.com/tijaraa/",
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+
+apiClient.interceptors.request.use((config)=>{
+    const token = localStorage.getItem("token");
+    if(token) config.headers.Authorization = `Bearer ${token}`
+    return config;
+})
+
+export default apiClient();
